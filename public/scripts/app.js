11063db889c75f3d13485e59aaa622588b19ef03
dissonance <ryanminor01u@yahoo.com>
2019-04-12 19:47:32 -0500

    made template two dynamic, going to put it into an object next

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,59 @@
 console.log("app.js is running!");
 
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Blah Blah Blah"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Thing one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Thing two"
+        )
+    )
 );
+
+var userName = "Ryan Minor";
+var userAge = 30;
+var userLocation = "San Diego";
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        userName.toUpperCase()
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        userAge
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        userLocation
+    )
+);
+
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 console.log("app.js is running!");
 
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+let template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>Blah Blah Blah</p>
+        <ol>
+        <li>Thing one</li>
+        <li>Thing two</li>
+        </ol>
+    </div>
+    );
+
+let userName = "Ryan Minor";
+let userAge = 30; 
+let userLocation = "San Diego";
+
+let templateTwo = (
+    <div>
+    <h1>{userName.toUpperCase()}</h1>
+    <p>Age: {userAge}</p>
+    <p>Location: {userLocation}</p>
+    </div>
+
+);
+
+
+const appRoot = document.getElementById("app");
+ReactDOM.render(templateTwo, appRoot);
